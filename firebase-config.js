3f6884e1c6417539e504b21218a8c77bc0bd6a14
Good luck <boo.boooda.booo@gmail.com>
2026-08-17 22:37:49 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "여기에_입력",
+  authDomain: "여기에_입력.firebaseapp.com",
+  databaseURL: "https://여기에_입력-default-rtdb.firebaseio.com",
+  projectId: "여기에_입력",
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
